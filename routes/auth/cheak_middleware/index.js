2fd25e79d90bb310975466880adcb145bb83a682
Mahin <k.m.mahin69@gmail.com>
2021-03-22 15:21:09 +0600

    https not

--- routes/auth/cheak_middleware/index.js
+++ routes/auth/cheak_middleware/index.js
@@ -8,8 +8,8 @@ module.exports =(req,res,next)=>{
     try{
         const data = UserData(req)
 
-        //  res.send({valid:true,data})
-            res.send("hell")
+         res.send({valid:true,data})
+  
     }catch{
         res.status(500).send("token not valid") 
     }  
