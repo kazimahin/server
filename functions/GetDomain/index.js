8325e4893df9d940d05e138d94693062e84019e2
kazimahin <k.m.mahin69@gmail.com>
2021-07-29 14:27:36 +0600

    proble m

--- functions/GetDomain/index.js
+++ functions/GetDomain/index.js
@@ -5,6 +5,7 @@ module.exports =  (req,res,next) =>{
     req.subdomain = req.hostname.split('.')[0] 
     req.domain = (process.env.MODE == "DEV") ? req.hostname.split('.')[0] +"."+process.env.DOMAIN :req.hostname
 
+    console.log(req.domain,req.subdomain);
     
     next()
 
